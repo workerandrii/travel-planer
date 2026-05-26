c72f716b86bcf4251db682876efdf190a5e5bf95
Laptop <workerandrii@gmail.com>
2026-05-26 13:32:12 +0300

    create project model

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
