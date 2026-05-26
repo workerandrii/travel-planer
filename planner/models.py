38de8ae55bdb066339b5552b503b953e43e0e9bb
Laptop <workerandrii@gmail.com>
2026-05-26 13:35:01 +0300

    create place model

--- planner/models.py
+++ planner/models.py
@@ -9,3 +9,12 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Place(models.Model):
+    project = models.ForeignKey(Project, related_name='places', on_delete=models.CASCADE)
+    external_id = models.IntegerField()
+    notes = models.TextField(blank=True, null=True)
+    is_visited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Place {self.external_id} in {self.project.name}"
