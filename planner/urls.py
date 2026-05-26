fa157d5de268c4bfca46418b28c8edd448d3054e
Laptop <workerandrii@gmail.com>
2026-05-26 15:08:04 +0300

    create view to retrive all project or create new project

---
+++ planner/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/projects/', views.project_list_create, name='project-list-create'),
+]
--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+# --- 1. PROJECT ENDPOINTS ---
+
+@api_view(['GET', 'POST'])
+def project_list_create(request):
+    """
+    GET: List all travel projects.
+    POST: Create a project (and optionally import a batch of places).
+    """
+    if request.method == 'GET':
+        projects = Project.objects.all().prefetch_related('places')
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
